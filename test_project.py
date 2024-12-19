80a5e599e5df3f3f1e97830f01f66886e8977008
Facundo <facundocomercio@outlook.com>
2024-12-19 15:53:29 -0300

    Construccion del README

--- test_project.py
+++ test_project.py
@@ -1,6 +1,7 @@
 
 from project import generador_progresion, grabar_progresion_MIDI, reproductor
 from unittest.mock import patch
+
 def test_generador_progresion():
     progresion = generador_progresion("A", "mayor")
     assert len(progresion) == 4
@@ -17,16 +18,15 @@ def test_grabar_progresion_MIDI(tmp_path):
 @patch("pygame.mixer.music")
 @patch("pygame.mixer")
 def test_reproductor(mock_mixer, mock_music):
-    # Configurar el mock
+    
     mock_mixer.init.return_value = None
     mock_music.load.return_value = None
     mock_music.play.return_value = None
-    mock_mixer.get_busy.side_effect = [True, False]  # Simula que el audio se está reproduciendo y luego termina
-
-    # Llamar a la función
+    mock_mixer.get_busy.side_effect = [True, False] 
+   
     reproductor("test_file.mid")
 
-    # Verificar que las funciones correctas fueron llamadas
+    
     mock_mixer.init.assert_called_once()
     mock_music.load.assert_called_once_with("test_file.mid")
     mock_music.play.assert_called_once()
